b504a427eb221ef5a89eea4939bdae2ade98736b
deepak1728 <deepakmatta28@gmail.com>
2025-06-12 19:28:27 -0500

    remove title from index.html

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "";
+  }, []);
   return (
     <div>
       <Header />
